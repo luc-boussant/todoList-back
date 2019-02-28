e72118c5f8264051aa699e66bca5cd92f6a8d363
lucb <luc.boussantr@gmail.com>
2019-02-28 15:31:28 +0100

    [Feature] :sparkles: Add task to taskList

--- src/taskList/taskList.service.ts
+++ src/taskList/taskList.service.ts
@@ -4,6 +4,7 @@ import { TaskListRepository } from './taskList.repository';
 import { CreateTaskDto } from '../task/interfaces/task.dto';
 import { TaskRepository } from '../task/task.repository';
 import { CreateTaskListDto } from './interfaces/taskList.dto';
+import { Task } from 'src/task/task.entity';
 
 @Injectable()
 export class TaskListService {
@@ -14,19 +15,13 @@ export class TaskListService {
 
   createTaskList = async (taskDto: CreateTaskListDto) => {
     const createdTaskList = await this.taskListRepository.save(taskDto);
-    const { ...createdTaskListDto } = createdTaskList;
-    return createdTaskListDto;
+    return createdTaskList;
   };
 
   addTask = async (id: string, taskDto: CreateTaskDto) => {
-    const task = await this.taskRepository.createTask(taskDto);
+    const task: Task = await this.taskRepository.createTask(taskDto);
     const taskList = await this.taskListRepository.findOneTaskList(id);
-    const { ...taskListDto } = taskList;
-    if (taskListDto.tasks) {
-      taskListDto.tasks.push(task);
-    } else {
-      taskListDto.tasks = [task];
-    }
+    const taskListDto = { ...taskList, tasks: [...taskList.tasks, task] };
     return await this.taskListRepository.updateTaskList(id, taskListDto);
   };
 }
